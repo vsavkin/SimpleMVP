47ab21a3ee70d5790b6559151639de01046247ba
Victor Savkin <vic.savkin@gmail.com>
2013-01-01 17:24:42 -0500

    removes all storage related logic from model_list

--- lib/src/model_list.dart
+++ lib/src/model_list.dart
@@ -3,21 +3,9 @@ part of simple_mvp;
 /**
 * The base class for model lists.
 */
-abstract class ModelList<T extends Model> {
+class ModelList<T extends Model> {
   final CollectionEvents on = new CollectionEvents();
-  final List<T> models = [];
-  Storage storage;
-
-  ModelList(){
-    storage = new Storage({"readAll" : rootUrl});
-  }
-
-  /**
-  * Abstract property to be overridden by subclasses. It's used by Storage.
-  */
-  String get rootUrl;
-
-  T makeInstance(Map attrs, ModelList list);
+  List<T> models = [];
 
   forEach(fn(T)) => models.forEach(fn);
 
@@ -25,7 +13,6 @@ abstract class ModelList<T extends Model> {
 
   void add(T model){
     models.add(model);
-    model.modelList = this;
     on.insert.dispatch(new CollectionInsertEvent(this, model));
   }
 
@@ -33,17 +20,12 @@ abstract class ModelList<T extends Model> {
     var index = models.indexOf(model);
     if(index == -1) return;
 
-    models.removeRange(index, 1);
+    models.removeAt(index);
     on.remove.dispatch(new CollectionRemoveEvent(this, model));
   }
 
   void reset(List list){
-    models.clear();
-    models.addAll(list.map((attrs) => makeInstance(attrs, this)));
+    models = list;
     on.load.dispatch(new CollectionLoadEvent(this));
-  }
-
-  void fetch(){
-    storage.readAll().then(reset);
   }
 }
