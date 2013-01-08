47394aeb2c96eec58bbc9e6aa8b67b0491cd64b2
Victor Savkin <vic.savkin@gmail.com>
2013-01-08 07:51:01 -0500

    Revert "adds optional event arg to presenter"
    This reverts commit b5bc6e5d6199083b96b70fddbf2f633bac940f72.

--- lib/src/presenter.dart
+++ lib/src/presenter.dart
@@ -47,7 +47,7 @@ class Presenter<T> {
     subscribeToDOMEvents();
   }
 
-  Presenter<T> render([event]){
+  Presenter<T> render(){
     if(template != null){
       el.innerHtml = template(model);
     }
