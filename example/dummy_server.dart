796108e665f12dd8247cc19610a417a57437aa9a
Victor Savkin <vic.savkin@gmail.com>
2013-01-11 20:01:15 -0500

    refactors dummy server to use default request handler for serving static resources

--- example/dummy_server.dart
+++ example/dummy_server.dart
@@ -7,46 +7,37 @@ final PORT = 8080;
 
 void main() {
   var server = new HttpServer();
-  server.addRequestHandler((request) => true, (request, response){
-    new RequestHandler(request,response).process();
-    }
-  );
+
+  var static = new StaticHandler("example");
+  server.defaultRequestHandler = static.onRequest;
+
+  var api = new ApiHandler();
+  server.addRequestHandler(api.match, api.onRequest);
+
   server.listen(HOST, PORT); 
 }
 
-class RequestHandler {
-  HttpRequest request;
-  HttpResponse response;
- 
-  RequestHandler(this.request, this.response);
+class ApiHandler {
+  bool match(request) => request.uri.startsWith("/api");
   
-  void process() {
+  void onRequest(request, response) {
     var uri = request.uri;
-    print("${request.method} ${uri}");
-      
-    if (uri.startsWith("/api/tasks") && request.method == "GET"){
-      _render("text/json", _items());
+    var method = request.method;
 
-    } else if (uri.startsWith("/api/tasks") && request.method == "POST"){
-      _renderNewRecord();
+    print("REQUEST: ${method} ${uri}");
+      
+    if (uri.startsWith("/api/tasks") && method == "GET"){
+      _render("text/json", _items(), response);
+
+    } else if (uri.startsWith("/api/tasks") && method == "POST"){
+      _renderNewRecord(request, response);
 
     } else if (uri.startsWith("/api/task")){
-      _renderUpdatedRecord();
-
-    } else if (uri.endsWith(".dart")) {
-      _renderDartFile();
-        
-    } else if (uri.endsWith(".css")){
-      var file = new File("example/${_filename(uri)}");
-      _render("text/css", file.readAsStringSync());
-  
-    } else if (uri.endsWith(".html")){
-      var file = new File("example/${_filename(uri)}");
-      _render("text/html", file.readAsStringSync());
+      _renderUpdatedRecord(request, response);
     }
   }
   
-  _renderNewRecord(){
+  _renderNewRecord(request, response){
     var s = new StringInputStream(request.inputStream);
     s.onData = (){
       var data = s.read();
@@ -54,42 +45,41 @@ class RequestHandler {
       
       var map = JSON.parse(data);
       map["id"] = new Random().nextInt(10000);
-      _render("text/json", JSON.stringify(map));
+      _render("text/json", JSON.stringify(map), response);
     };    
   }
 
-  _renderUpdatedRecord(){
+  _renderUpdatedRecord(request, response){
     var s = new StringInputStream(request.inputStream);
     s.onData = (){
       var data = s.read();
-      print("body: ${data}");
-      _render("text/json", data);
+      print("BODY: ${data}");
+      _render("text/json", data, response);
     };
   }
-  
-  _renderDartFile(){
-    var uri = request.uri;
 
-    var libFile = new File(_filename(uri));
-    var exampleFile =  new File("example/${_filename(uri)}");
-    
-    if(libFile.existsSync()){
-      _render("application/dart", libFile.readAsStringSync());
-    } else {
-      _render("application/dart", exampleFile.readAsStringSync());
-    }
-  }
-  
-  _filename(uri) => uri.substring(uri.indexOf("/") + 1);
-  
-  _render(contentType, body){
+  _render(contentType, body, response){
     response.headers.set(HttpHeaders.CONTENT_TYPE, "$contentType; charset=UTF-8");
     response.outputStream.writeString(body);
     response.outputStream.close();
   }
 
   _items() {
-    var r = [{"id": 1, "text": "Task 1", "status" : "inProgress"}, {"id": 2, "text": "Task 2", "status" : "inProgress"}];
+    var r = [
+        {"id": 1, "text": "Task 1", "status" : "inProgress"},
+        {"id": 2, "text": "Task 2", "status" : "inProgress"}
+    ];
     return JSON.stringify(r);
+  }
+}
+
+class StaticHandler {
+  String folder;
+
+  StaticHandler(this.folder);
+
+  onRequest(request, response) {
+    var file = new File('${folder}${request.path}');
+    file.exists().then((_) => file.openInputStream().pipe(response.outputStream));
   }
 }
